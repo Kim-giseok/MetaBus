fcf392bbd776a37cdf15418a3b5145530acc73a9
Kim-giseok <belbeck@naver.com>
2025-02-18 12:50:00 +0900

    Feat: 카메라 이동 구현
    카메라가 플레이어를 따라 부드럽게 이동합니다.

---
+++ Assets/Scripts/MainScene/Entity/MapCameraControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCameraControl : MonoBehaviour
+{
+    public Transform target;
+    float offsetX, offsetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null) return;
+
+        offsetX = transform.position.x - target.position.x;
+        offsetY = transform.position.y - target.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+        Vector3 pos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
+    }
+}
