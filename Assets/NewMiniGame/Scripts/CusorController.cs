cb6ad5ebe1f4e88db8d81a03df622346279d6593
Kim-giseok <belbeck@naver.com>
2025-02-19 15:03:33 +0900

    Feat: 마우스 커서 생성
    마우스의 위치를 알기 위한 커서를 생성했습니다.

--- Assets/NewMiniGame/Scripts/CusorController.cs
+++ Assets/NewMiniGame/Scripts/CusorController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CusorController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Camera camera;
+
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnLook(InputValue inputValue)
     {
-        
+        Vector2 mousePosition = inputValue.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+
+        transform.position = worldPos;
     }
 }
