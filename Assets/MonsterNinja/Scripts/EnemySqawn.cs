f00bcfa9ea918e6b3e03b1585aa1050e6ce5ad68
Kim-giseok <belbeck@naver.com>
2025-02-19 20:34:38 +0900

    Refactor: UI업데이트 코드 수정
    if문 바깥으로 꺼내 가끔 한 프레임 끊길 수 있는 문제를 대비했습니다.

--- Assets/MonsterNinja/Scripts/EnemySqawn.cs
+++ Assets/MonsterNinja/Scripts/EnemySqawn.cs
@@ -33,8 +33,8 @@ public class EnemySqawn : MonoBehaviour
         else
         {
             delayTime -= Time.deltaTime;
-            UIManager.Instance.UpdateTime(curTime, maxTime);
         }
+        UIManager.Instance.UpdateTime(curTime, maxTime);
     }
 
     public void SpawnEnemy()
