de03d8c5b4129fb422bc02ec45ced20d1d5c3817
Kim-giseok <belbeck@naver.com>
2025-02-19 11:31:37 +0900

    Refact: GameEnter코드 수정
    플레이어를 확인하는 부분을 태그를 이용하는 것으로 수정했습니다.

--- Assets/Scripts/MainScene/Entity/GameEnter.cs
+++ Assets/Scripts/MainScene/Entity/GameEnter.cs
@@ -6,12 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class GameEnter : MonoBehaviour
 {
-    [SerializeField] GameObject player;
     [SerializeField] string gameName;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject == player)
+        if (collision.gameObject.CompareTag("Player"))
             SceneManager.LoadScene(gameName);
     }
 }
