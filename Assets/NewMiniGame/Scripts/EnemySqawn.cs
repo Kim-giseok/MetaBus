8b6077365de09667406ad3a1ba6317a4324be9b9
Kim-giseok <belbeck@naver.com>
2025-02-19 16:03:27 +0900

    Feat: 시잔제한 추가
    제한시간이 지나면 게임오버가 됩니다.

--- Assets/NewMiniGame/Scripts/EnemySqawn.cs
+++ Assets/NewMiniGame/Scripts/EnemySqawn.cs
@@ -15,13 +15,21 @@ public class EnemySqawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(delayTime <= 0)
+        if (curTime < 0)
+        {
+            curTime = 0;
+            GameManager.instance.GameOver();
+            return;
+        }
+        curTime -= Time.deltaTime;
+
+        if (delayTime <= 0)
         {
             SpawnEnemy();
             delayTime = Random.Range(3f, 6f);
@@ -30,18 +38,14 @@ public class EnemySqawn : MonoBehaviour
         {
             delayTime -= Time.deltaTime;
         }
-        if (curTime > 0)
-            curTime -= Time.deltaTime;
-        else
-            curTime = 0f;
     }
 
     public void SpawnEnemy()
     {
-        _rigEnemy = Instantiate(enemyPrefebs[Random.Range(0, enemyPrefebs.Length)],this.transform).GetComponent<Rigidbody2D>();
+        _rigEnemy = Instantiate(enemyPrefebs[Random.Range(0, enemyPrefebs.Length)], this.transform).GetComponent<Rigidbody2D>();
 
         _rigEnemy.transform.localPosition += new Vector3(Random.Range(-4, 4), 0, 0);
-        _rigEnemy.AddForce(Vector2.up * Random.Range(600f,800f));
+        _rigEnemy.AddForce(Vector2.up * Random.Range(600f, 800f));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
