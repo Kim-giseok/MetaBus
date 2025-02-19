1694c563cddcdb895993a79994e0c503a59435be
Kim-giseok <belbeck@naver.com>
2025-02-19 21:07:49 +0900

    Fix: 애니메이션 출력 수정
    지웠던 미정의 오버라이드 메서드때문에 애니메이션이 안나오는 문제를 해결했습니다.

--- Assets/Scripts/Entity/RideController.cs
+++ Assets/Scripts/Entity/RideController.cs
@@ -10,9 +10,13 @@ public class RideController : BaseController
         this.lookDir = lookDir;
     }
 
+    protected virtual void FixedUpdate()
+    {
+    }
+
     void OnMove(InputValue inputValue)
     {
         lookDir = inputValue.Get<Vector2>().magnitude >= 1 ? inputValue.Get<Vector2>() : lookDir;
-        animHandller.Move(inputValue.Get<Vector2>());
+        animHandller.Move(inputValue.Get<Vector2>().normalized);
     }
 }
